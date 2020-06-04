4d27ad5d6b01d1acbce256fe00da0e517b97530e
hamidafghan <abd.hamidafghan@gmail.com>
2020-06-04 11:15:30 +0430

    List of all permissions

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -22,7 +22,34 @@ class PermissionsTableSeeder extends Seeder
         Permission::create(['name' => 'Export Complaints']);
 
         // Project
+        Permission::create(['name' => 'Create Project']);
+        Permission::create(['name' => 'View Projects']);
+        Permission::create(['name' => 'Delete Project']);
+        Permission::create(['name' => 'Edit Project']);
 
         // Program
+        Permission::create(['name' => 'Create Program']);
+        Permission::create(['name' => 'View Programs']);
+        Permission::create(['name' => 'Delete Program']);
+        Permission::create(['name' => 'Edit Program']);
+
+        // Users
+        Permission::create(['name' => 'View Users']);
+        Permission::create(['name' => 'Delete User']);
+        Permission::create(['name' => 'Edit User']);
+        Permission::create(['name' => 'Assign Role']);
+
+        // Roles
+        Permission::create(['name' => 'Create Role']);
+        Permission::create(['name' => 'View Roles']);
+        Permission::create(['name' => 'Delete Role']);
+        Permission::create(['name' => 'Edit Role']);
+        Permission::create(['name' => 'Grant Permissions']);
+
+        // Settings
+        Permission::create(['name' => 'View Activity Log']);
+        Permission::create(['name' => 'View Backups']);
+        Permission::create(['name' => 'Download Backup']);
+        Permission::create(['name' => 'Create Backup']);
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,43 +15,56 @@
         <p class="text-gray-400 text-base ml-2">Dashboard</p>
     </a>
 
-    <a class="@if(request()->is('complaints*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('complaints') }}">
-        <img src="{{ url('/images/complaint-icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Complaints</p>
-    </a>
+    @can('View Complaints')
+        <a class="@if(request()->is('complaints*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('complaints') }}">
+            <img src="{{ url('/images/complaint-icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Complaints</p>
+        </a>
+    @endcan
 
-    <a class="@if(request()->is('projects*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('/projects') }}">
-        <img src="{{ url('/images/project-icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Projects</p>
-    </a>
+    @can('View Projects')
+        <a class="@if(request()->is('projects*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('/projects') }}">
+            <img src="{{ url('/images/project-icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Projects</p>
+        </a>
+    @endcan
 
-    <a class="@if(request()->is('programs*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('/programs') }}">
-        <img src="{{ url('/images/program-icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Programs</p>
-    </a>
+    @can('View Programs')
+        <a class="@if(request()->is('programs*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('/programs') }}">
+            <img src="{{ url('/images/program-icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Programs</p>
+        </a>
+    @endcan
+
 
     <div class="bg-gray-300 w-2/3 my-px my-1">
         <hr>
     </div>
 
-    <a class="@if(request()->is('users*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('/users') }}">
-        <img src="{{ url('/images/users_icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Users</p>
-    </a>
+    @can('View Users')
+        <a class="@if(request()->is('users*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('/users') }}">
+            <img src="{{ url('/images/users_icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Users</p>
+        </a>
+    @endcan
 
-    <a class="@if(request()->is('roles*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('/roles') }}">
-        <img src="{{ url('/images/role_icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Roles</p>
-    </a>
+    @can('View Roles')
+        <a class="@if(request()->is('roles*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('/roles') }}">
+            <img src="{{ url('/images/role_icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Roles</p>
+        </a>
+    @endcan
 
-    <a class="@if(request()->is('settings*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
-       href="{{ url('/settings') }}">
-        <img src="{{ url('/images/setting-icon.png') }}" class="w-5" alt="">
-        <p class="text-gray-400 text-base ml-2">Setting</p>
-    </a>
+    @if(Auth::user()->can('View Activity Log') || Auth::user()->can('View Backups'))
+        <a class="@if(request()->is('settings*')) bg-blue-lighter @endif hover:bg-blue-lighter flex items-center w-full p-1 rounded my-2 inline-block"
+           href="{{ url('/settings') }}">
+            <img src="{{ url('/images/setting-icon.png') }}" class="w-5" alt="">
+            <p class="text-gray-400 text-base ml-2">Setting</p>
+        </a>
+    @endcan
 </div>
